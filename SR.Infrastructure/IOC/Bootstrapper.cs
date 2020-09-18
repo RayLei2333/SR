45e8577d67dfc9be02b8228554f2971bd5ff5888
Ray <1047507300@qq.com>
2020-09-18 20:34:44 +0800

    Ray
    创建项目

---
+++ SR.Infrastructure/IOC/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace SR.Infrastructure.IOC
+{
+    public class Bootstrapper
+    {
+        public static void SetAutofacWebAPI()
+        {
+            var builder = new ContainerBuilder();
+
+            Assembly[] controllers = new Assembly[] {
+                Assembly.Load("HARSON.API")
+            };
+            builder.RegisterControllers(controllers);
+
+            builder.RegisterAssemblyTypes(new Assembly[] {
+                Assembly.Load("HARSON.Business"),
+                Assembly.Load("HARSON.Business.Imp")
+            }).AsImplementedInterfaces();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));//这句最重要，提供注入点
+        }
+    }
+}
---
+++ SR.Infrastructure/IOC/ObjectFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SR.Infrastructure.IOC
+{
+    public class ObjectFactory
+    {
+        public static T GetObject<T>()
+        {
+            //T result = (T)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(T));
+            T result = DependencyResolver.Current.GetService<T>();
+            return result;
+        }
+
+        public static IEnumerable<T> GetObjects<T>()
+        {
+            IEnumerable<T> result = DependencyResolver.Current.GetServices<T>();
+            return result;
+        }
+    }
+}
